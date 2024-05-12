97b1d6e85b84d42418e7a0b4d5914dec8ef6f0c5
Ulfat Zakirli <ulfetzakirli@gmail.com>
2024-05-12 14:43:25 +0400

    feat: introduction

---
+++ classwork/01.Introduction/script.js
@@ -0,0 +1,149 @@
+// keywords- var,let, const
+// const user = 'Ulfat Zakirli'
+// var -keyword
+// user- variable
+// "Aysu"- value
+
+// console.log(user);
+
+// {
+//     const city = 'Gence'
+//     console.log(city);
+// }
+// {
+//     const city = 'Baku'
+// }
+
+
+// named conversions
+// 1.camelCase
+// 2.PascalCase
+// 3.snake_case
+// 4.kebab-case
+
+// let first-name = 'Ulfat'
+// console.log(firstName)
+
+// let FirstName = 'Ulfat'
+// console.log(FirstName)
+
+// let first_name = 'Ulfat'
+// console.log(first_name)
+
+// let first-name = 'Ulfat'
+// console.log(first-name)
+
+
+// let firstName = 'Ulfat'
+// let lastName = 'Zakirli'
+
+// let fullName = `name:${firstName} surname:${lastName}`
+// console.log(fullName);
+// firstName,lastName,fatherName,age(let)
+
+// fullInfo name:${firstName}  etc...
+
+// let age = 25
+// let age2 = 'ulfat'
+// console.log(age - age2);
+// NaN- not a number
+
+// *,-,/,+,**,% Math operators
+// + concat
+
+// console.log(1 / 1 / 'string');
+// console.log('1' * 1 * 4 * 1 * 1);
+
+
+// console.log(5 ** 3);
+// console.log(25 % 5);
+
+// let a,b,c
+// let a = 1
+// let b = '1'
+// let c = 1
+
+// console.log(1 + '1' + 1);
+// b + a + c
+// c + b + a
+// c + a + b
+
+
+// console.log(16 > 9);
+
+// Data types JS -primitive and non-primitive
+
+// primitive
+// string, bigInt, number,boolean,Symbol, null,undefined
+
+// non
+// object
+
+// string
+// '6'
+// 'ulfat'
+// 'false'
+// 'null'
+
+
+// to check typeof value - typeof
+// number all numbers (post, negat,0) - 6,9,1000,-9,0
+
+// boolean - true and false
+
+// let a = Symbol(null)
+// console.log(a);
+
+
+// conditinal if, else
+
+// = assignment
+
+// == (only value)  !=
+// === (value and type) !==
+
+// console.log(4 === '4');
+// console.log(4 === 4);
+
+
+
+// || - or
+// && - and
+// !
+
+let age = 24
+let firstName = 'Ulfat'
+let lastName = 'Zakirli'
+
+// 1
+if (age == 24) {
+    console.log('1');
+} else if (age === 20) {
+    console.log('2');
+}
+else if (age === 20) {
+    console.log('3');
+}
+else {
+    console.log('4');
+}
+
+// 2
+if (age == 19 || user !== 'Ulfat') {
+    console.log('1');
+} else if (age === 20) {
+    console.log('2');
+}
+else if (age === 20) {
+    console.log('3');
+}
+else {
+    console.log('4');
+}
+
+
+
+
+
+
+
