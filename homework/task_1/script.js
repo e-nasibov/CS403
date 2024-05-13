4391f95ba1fdf9049a98334f17a1a9df68c1fade
Ulfat Zakirli <ulfetzakirli@gmail.com>
2024-05-13 17:10:01 +0400

    feat: task 1

---
+++ homework/task_1/script.js
@@ -0,0 +1,66 @@
+////////////// TASK 1 //////////////
+
+// 1.Multiply 12 with 8, and console the result:
+
+// 2.Divide 10 by 2 add result to variables x, and console x:
+
+// 3.Create two variables called num1 and num2, assign the value 20 to num1 and value 17 to num2 then addition(+) them in console.
+
+/* 4.Create variables called name, surname and year assign values to them ('your name', 'your surname', ' your birth of year'),
+  then add them to varibale name (info) and console info. */
+
+// 5.Console the remainder(%) when 17 is divided by 12.
+
+// 6.Create a varibale called "city name" (camelCase). Assign value Baku, then call variable again
+//  and reassign value Gence,console result.
+
+
+
+////// TASK 2 //////////////
+
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++;
+
+// console.log(c); // ?
+// console.log(d); // ?
+// console.log(b); // ?
+
+
+////////////// TASK 3 //////////////
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x); // ?
+
+
+////////////// TASK 4 //////////////
+
+// console.log('test1', test); // ?
+// {
+//     let test = "something"
+//     console.log('test2', test); // ?
+// }
+// console.log('test3', test);  // ?
+
+
+// console.log('test1', test);  // ?
+// {
+//     var test = "something"
+//     console.log('test2', test); // ?
+// }
+// console.log('test3', test); // ?
+
+
+////////////// TASK 5 //////////////
+
+//Find the type of all the following cases
+
+// let name = "Ulfat"
+// let surname = Zakirli
+// let year = 2000
+// year = "2000"
+// let city;
+// let qualification = null
+// let obj = { name: 'ulfat' }
+// let arr = ['a', 'b', 'c']
