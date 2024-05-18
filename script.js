8ea2838461e727a65afa9684b970ccd3b8517a40
e-nasibov <e.nesibov@gmail.com>
2024-05-18 23:06:49 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+//TASK1
+
+//1.Multiply 12 with 8, and console the result:
+
+let num1 = 8
+let num2 = 12
+let result = num1 * num2
+console.log(result);
+
+
+// 2.Divide 10 by 2 add result to variables x, and console x:
+
+let n1 = 10
+let n2 = 2
+let result1 = n1 / n2
+console.log(result1);
+
+
+
+// 3.Create two variables called num1 and num2 assign the value 20 to num1 and value 17
+
+let nu1 = 20
+let nu2 = 17
+let sum = nu1 + nu2
+console.log(sum);
+
+
+
+// //4.Create variables called name, surname and year assign values to them name and surname
+// let namee = kapital;
+// let surname = nasibov;
+// let year = "1991";
+// let result = namee + " " + surname + " " + year;
+// console.log(result);
+
+
+//5.Console the remainder(%) when 17 is divided by 12.
+
+let n3 = 17
+let n4 = 12
+let bolmek = n2/n3
+console.log(bolmek);
+
+
+
+//6.Create a varibale called "city name" (camelCase). Assign value Baku, then call variable again
+//  and reassign value Gence,console result.
+
+let cityName = "Baku";
+console.log(cityName);
+
+
+////// TASK 2 //////////////
+
+let a = 1, b = 1;
+let c = ++a;
+let d = b++;
+
+console.log(c); 
+console.log(d); 
+console.log(b);
+
+////////////// TASK 3 //////////////
+
+// let a = 2;
+// let x = 1 + (a * 2);
+// console.log(x);
+
+////////////// TASK 4 //////////////
+
+// //console.log('test1', test);
+// {
+// let test = "something"
+// console.log('test2', test);
+// }
+// console.log('test3', test);
+
+////////////// TASK 5 //////////////
+
+//Find the type of all the following cases
+
+let name1 = "elshan"
+let surname = "nasibov"
+let year = 1991
+year = "1991"
+let city;
+let qualification = null
+let obj = { name: 'elshan' }
+let arr = ['a', 'b', 'c']
+
+console.log(typeof name1);
+console.log(typeof surname);
+console.log(typeof year);
+console.log(typeof city);
+console.log(typeof qualification);
+console.log(typeof obj);
+console.log(typeof arr);
